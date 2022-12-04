6fa82427fba673b52a1053d9f360e8fddea4cbd5
Emil Ahlberg <emil.ahlberg1@gmail.com>
2022-12-04 12:32:49 +0100

    init

---
+++ internal/aoc_helpers.go
@@ -0,0 +1,33 @@
+package aoc
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const verbose = false
+
+func LoadFile(name string) []string {
+	file, err := os.Open(fmt.Sprintf("../input/%s.txt", name))
+	defer file.Close()
+
+	var rows []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		rows = append(rows, scanner.Text())
+	}
+
+	if err != nil {
+		fmt.Println("yippie ki-yay", name)
+		os.Exit(1)
+	}
+
+	if verbose {
+		for _, row := range rows {
+			fmt.Println(row)
+		}
+	}
+
+	return rows
+}

ebe945a1bc8cb4b2a83a6b6f3ffc76a91dd1c6ca
Emil Ahlberg <emil.ahlberg1@gmail.com>
2022-12-04 14:52:00 +0100

    cleanup

--- internal/04/solution.go
+++ internal/04/solution.go
@@ -8,15 +8,12 @@ import (
 func Solve(rows []string) (part1, part2 int) {
 	for _, row := range rows {
 		ranges := strings.Split(row, ",")
-		elf1 := strings.Split(ranges[0], "-")
-		elf2 := strings.Split(ranges[1], "-")
+		elf1, elf2 := strings.Split(ranges[0], "-"), strings.Split(ranges[1], "-")
 		lb1, _ := strconv.Atoi(elf1[0])
 		lb2, _ := strconv.Atoi(elf2[0])
 		ub1, _ := strconv.Atoi(elf1[1])
 		ub2, _ := strconv.Atoi(elf2[1])
-		if lb1 <= lb2 && ub1 >= ub2 {
-			part1++
-		} else if lb2 <= lb1 && ub2 >= ub1 {
+		if lb1 <= lb2 && ub1 >= ub2 || lb2 <= lb1 && ub2 >= ub1 {
 			part1++
 		}
 		if !(lb1 > ub2 || ub1 < lb2) {
